7e90f49d5fb4128afe9a7d82cb6544b687093822
markus812498 <markus@84codes.com>
2025-03-26 10:29:52 +1300

    new structure

---
+++ python-mqtt-example/README.md
@@ -0,0 +1,3 @@
+This sample app shows how to connect with [LavinMQ](https://lavinmq.com/) and [RabbitMQ](https://www.rabbitmq.com/docs/mqtt) using [CloudAMQP](https://www.cloudamqp.com/) and both publish and subscribe messages. 
+
+    $ pip install -r requirements.txt
---
+++ python-mqtt-example/app.py
@@ -0,0 +1,57 @@
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import paho.mqtt.client as mqtt
+from urllib.parse import urlparse
+import os
+
+# Define event callbacks
+def on_connect(client, userdata, flags, rc):
+    print("rc: " + str(rc))
+
+def on_message(client, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+def on_publish(client, obj, mid):
+    print("mid: " + str(mid))
+
+def on_subscribe(client, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(client, obj, level, string):
+    print(string)
+
+mqttc = mqtt.Client()
+# Assign event callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+# Uncomment to enable debug messages
+mqttc.on_log = on_log
+
+# Parse BROKER_URL(or fallback to localhost)
+url_str = os.environ.get('BROKER_URL', 'mqtt://guest:guest@localhost:1883/vhost')
+url = urlparse(url_str)
+topic = 'test'
+vhost = url.path[1:] or url.username
+
+# Connect
+mqttc.username_pw_set(username=f"{vhost}:{url.username}", password=url.password)
+mqttc.connect(url.hostname, url.port)
+
+# Start subscribe, with QoS level 0
+mqttc.subscribe(topic, 0)
+
+# Publish a message
+mqttc.publish(topic, "my message")
+
+# Continue the network loop, exit when an error occurs
+rc = 0
+while rc == 0:
+    rc = mqttc.loop()
+print("rc: " + str(rc))
